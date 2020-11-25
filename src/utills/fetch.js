c9137b7aa3a23e0f715f1561a304b82d592e8c58
unknown <lua.barbosa@yahoo.com.br>
2020-11-24 23:41:09 -0300

    feat: Criação de Fetch - API

---
+++ src/utills/fetch.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+ export function fazerRequisicaoComBody(url, metodo, conteudo, token) {
+  return fetch(url, {
+    method: metodo,
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: token && `Bearer ${token}`,
+    },
+    body: JSON.stringify(conteudo),
+  });
+}
