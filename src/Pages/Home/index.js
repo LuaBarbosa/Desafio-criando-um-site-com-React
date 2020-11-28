e9075a1609337c5eb86eaffdedfe5b247c2938a3
unknown <lua.barbosa@yahoo.com.br>
2020-11-28 10:16:50 -0300

    feat: Incluindo pagina home

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -1,9 +1,34 @@
 import React from "react";
 
+import logout from "./logout.svg";
+import css from "./style.css";
+import Logo from "./logo.svg";
+import home from "./home.svg";
+import cobranças from "./cobrancas.svg";
+import clientes from "./clientes.svg";
+
 export default function Home(props) {
   return (
     <div className="Home">
-      <h1>Hello World!!!</h1>
+      <div className="barraLateral">
+        <label>
+          <img src={Logo} />
+          <div className="menu">
+            <img src="home" /> Home
+            <img src="cobrancas" /> Cobranças
+            <img src="clientes" /> Clientes
+          </div>
+          <button>Criar cobranças</button>
+        </label>
+      </div>
+      <div className="corpoPrincipal">
+        <div className="header">
+          <label className="fundo">
+            <label className="saldo">Saldo em conta R$ 0,00</label>
+            <img src={logout} />
+          </label>
+        </div>
+      </div>
     </div>
   );
 }
