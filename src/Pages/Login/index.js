cd5ccc3442ce19330e70f1f2e94e8c18936fb0c8
unknown <lua.barbosa@yahoo.com.br>
2020-12-02 11:07:38 -0300

    fix: Correções em login

--- src/Pages/Login/index.js
+++ src/Pages/Login/index.js
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useContext } from "react";
 import { fazerRequisicaoComBody } from "../../utills/fetch";
-import { BrowserRouter } from "react-router-dom";
-import History from "../../history";
-import style from "../Login/style.css";
+import { Link } from "react-router-dom";
+import History from "../../utills/history";
+import StoreContext from "../../components/Store/Context";
+import "./style.css";
 
 import Logo from "../Login/Logo.svg";
 import Olho from "./olho.svg";
@@ -11,70 +12,67 @@ import OlhoC from "./olhoc.svg";
 export default function Login() {
   const [email, setEmail] = React.useState("");
   const [senha, setSenha] = React.useState("");
-  const [token, setToken] = React.useState("null");
+  const { setToken } = useContext(StoreContext);
   const [senhaVisivel, setSenhaVisivel] = React.useState(false);
 
   return (
     <div className="login">
-      <BrowserRouter>
-        <form
-          onSubmit={(event) => {
-            event.preventDefault();
-            fazerRequisicaoComBody(
-              "https://cubos-desafio-4.herokuapp.com/auth",
-              "POST",
-              { email, senha }
-            )
-              .then((res) => res.json())
-              .then((resposta) => {
-                console.log(resposta);
-                const token = resposta.dados.token;
-                if (token) {
-                  setToken(token);
-
-                  History.push("/home");
-                } else {
-                  alert(" Erro ");
-                }
-              });
-          }}
-        >
-          <label>
-            <img src={Logo} />
-            <div className="dadosLogin"></div>
-            E-mail
+      <form
+        onSubmit={(event) => {
+          event.preventDefault();
+          fazerRequisicaoComBody(
+            "https://cubos-desafio-4.herokuapp.com/auth",
+            "POST",
+            { email, senha }
+          )
+            .then((res) => res.json())
+            .then((resposta) => {
+              console.log(resposta);
+              const token = resposta.dados.token;
+              if (token) {
+                setToken(token);
+                History.push("/home");
+              } else {
+                alert(" E-mail ou senha incorretos ");
+              }
+            });
+        }}
+      >
+        <label>
+          <img src={Logo} />
+          <div className="dadosLogin"></div>
+          E-mail
+          <input
+            type="email"
+            placeholder="exemplo@gmail.com"
+            value={email}
+            onChange={(event) => setEmail(event.target.value)}
+          />
+          Senha
+          <div className="inputEnvelop">
             <input
-              type="email"
-              placeholder="exemplo@gmail.com"
-              value={email}
-              onChange={(event) => setEmail(event.target.value)}
+              onChange={(event) => {
+                setSenha(event.target.value);
+              }}
+              className="login"
+              type={senhaVisivel ? "text" : "password"}
             />
-            Senha
-            <div className="inputEnvelop">
-              <input
-                onChange={(event) => {
-                  setSenha(event.target.value);
-                }}
-                className="login"
-                type={senhaVisivel ? "text" : "password"}
-              />
-              <img
-                src={senhaVisivel ? OlhoC : Olho}
-                onClick={() => {
-                  setSenhaVisivel(!senhaVisivel);
-                }}
-                alt="Ver Senha"
-              />
-            </div>
-            Esqueci a senha
-            <button>Entrar</button>
-          </label>
-          <div className="cadastre">
-            Não tem conta?{" "}
-            <button className="cadastre-se"> Cadastre-se!</button>
+            <img
+              src={senhaVisivel ? OlhoC : Olho}
+              onClick={() => {
+                setSenhaVisivel(!senhaVisivel);
+              }}
+              alt="Ver Senha"
+            />
           </div>
-        </form>
-      </BrowserRouter>
+          Esqueci a senha
+          <button>Entrar</button>
+        </label>
+        Não tem conta?
+        <div className="cadastre">
+          <Link to="/usuarios"> Cadastre-se </Link>
+        </div>
+      </form>
     </div>
   );
 }
