1653486416eec96514b68a34968cf140bb0b856e
unknown <lua.barbosa@yahoo.com.br>
2020-12-02 21:33:52 -0300

    feat: Incluindo nova requisição

--- src/utills/fetch.js
+++ src/utills/fetch.js
@@ -1,6 +1,6 @@
 import React from "react";
 
- export function fazerRequisicaoComBody(url, metodo, conteudo, token) {
+export function fazerRequisicaoComBody(url, metodo, conteudo, token) {
   return fetch(url, {
     method: metodo,
     headers: {
@@ -8,5 +8,15 @@ import React from "react";
       Authorization: token && `Bearer ${token}`,
     },
     body: JSON.stringify(conteudo),
+  });
+}
+
+export function fazerRequisicao(url, metodo, token) {
+  return fetch(url, {
+    method: metodo,
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: token && `Bearer ${token}`,
+    },
   });
 }
