07766fa5fb046bdf61144a7c4192b98899ab57a8
unknown <lua.barbosa@yahoo.com.br>
2020-12-03 00:31:41 -0300

    feat: inclusão de click no button

--- src/Pages/Logout/index.js
+++ src/Pages/Logout/index.js
@@ -1,11 +1,18 @@
-import React from "react";
+import React, { useContext } from "react";
 import logout from "../../assets/logout.svg";
+import StoreContext from "../../components/Store/Context";
 import "./style.css";
 
-const Logout = (props) => {
+const Logout = () => {
+  const { setToken } = useContext(StoreContext);
   return (
     <>
-      <button className="logout" onClick={props.onClick}>
+      <button
+        className="logout"
+        onClick={() => {
+          setToken(null);
+        }}
+      >
         <img src={logout} alt="Deslogar" />
       </button>
     </>
