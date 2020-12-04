c3e7a09a89fa0392bcafc77caf3d66ae4f1dc5f1
unknown <lua.barbosa@yahoo.com.br>
2020-12-04 15:21:00 -0300

    feat: Adicionando dados na Lista de cobranças

--- src/Pages/Listar cobrança/index.js
+++ src/Pages/Listar cobrança/index.js
@@ -1,17 +1,37 @@
-import React, { useContext } from "react";
+import React from "react";
+import boleto from "../../assets/boleto.svg";
 import "./style.css";
 
-export default ListarCobranca(props){
-return (
-	<div className="tabela">
-	<table>
-	  <thead>
-		<th>Cliente</th>
-		<th>Descrição</th>
-		<th>Valor</th>
-		<th>Status</th>
-		<th>Vencimento</th>
-		<th>Boleto</th>
-	  </thead>
-);
+export default function ListarCobranca(props) {
+  return (
+    <div className="tabela">
+      <table>
+        <thead>
+          <th>Cliente</th>
+          <th>Descrição</th>
+          <th>Valor</th>
+          <th>Status</th>
+          <th>Vencimento</th>
+          <th>Boleto</th>
+        </thead>
+        <tbody>
+          {props.cobranca &&
+            props.cobranca.map((cobranca) => (
+              <tr>
+                <td>{cobranca.idDoCliente}</td>
+                <td>{cobranca.descricao}</td>
+                <td>{cobranca.valor}</td>
+                <td>{cobranca.status}</td>
+                <td>{cobranca.vencimento}</td>
+                <td>
+                  <button>
+                    <img src={boleto} alt="Visualizar boleto" />
+                  </button>
+                </td>
+              </tr>
+            ))}
+        </tbody>
+      </table>
+    </div>
+  );
 }
