b23e05c29ee406bf6599c7e12e310f13a0eee440
unknown <lua.barbosa@yahoo.com.br>
2020-12-04 10:23:12 -0300

    fix: Corrigindo erros

--- src/Pages/Clientes/index.js
+++ src/Pages/Clientes/index.js
@@ -7,15 +7,19 @@ import PaginaPrincipal from "../Pagina Principal";
 import Header from "../Header";
 import StoreContext from "../../components/Store/Context";
 
-import lupa from "../../assets/lupa.svg";
+import ListarClientes from "../Listar Clientes";
+import BuscarCliente from "../Buscar Cliente";
 
 export default function Clientes() {
   const [clientes, setClientes] = React.useState();
+  const [busca, setBusca] = React.useState();
   const { token } = useContext(StoreContext);
 
   React.useEffect(() => {
     fazerRequisicao(
-      "https://cubos-desafio-4.herokuapp.com/clientes?clientesPorPagina=10&offset=0",
+      `https://pagamentos-back.herokuapp.com/clientes?${
+        busca ? `busca=${busca}&` : ""
+      }clientesPorPagina=10&offset=0`,
       "GET",
       token
     )
@@ -26,8 +30,7 @@ export default function Clientes() {
           setClientes(resposta.dados.clientes);
         }
       });
-  }, []);
-  console.log(clientes, "clientes");
+  }, [busca]);
   return (
     <div className="Clientes">
       <PaginaPrincipal />
@@ -45,56 +48,10 @@ export default function Clientes() {
               Adicionar Cliente
             </button>
           </div>
-          <div className="buscar">
-            <input placeholder="Procurar por nome, e-mail e CPF"></input>
-            <button>
-              <img src={lupa} />
-              BUSCAR
-            </button>
-          </div>
+          <BuscarCliente busca={busca} setBusca={setBusca} />
         </div>
         <div className="tabela">
-          <table>
-            <thead>
-              <th>Cliente</th>
-              <th>Cobranças Feitas</th>
-              <th>Cobranças Recebidas</th>
-              <th>Status</th>
-            </thead>
-            <tbody>
-              {clientes &&
-                clientes.map((cliente) => (
-                  <tr>
-                    <td>
-                      {cliente.nome}
-                      {cliente.email}
-                      {cliente.telefone}
-                    </td>
-                    <td>
-                      {Number(cliente.cobrancasFeitas || "0").toLocaleString(
-                        "pt-br",
-                        {
-                          style: "currency",
-                          currency: "BRL",
-                        }
-                      )}
-                    </td>
-                    <td>
-                      {Number(cliente.cobrancasRecebidas || "0").toLocaleString(
-                        "pt-br",
-                        {
-                          style: "currency",
-                          currency: "BRL",
-                        }
-                      )}
-                    </td>
-                    <td>
-                      {cliente.estaInadimplente ? "Inadimplente" : "Em dias"}
-                    </td>
-                  </tr>
-                ))}
-            </tbody>
-          </table>
+          <ListarClientes clientes={clientes} />
         </div>
       </div>
     </div>
