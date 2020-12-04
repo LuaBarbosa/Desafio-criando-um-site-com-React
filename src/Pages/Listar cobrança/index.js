0c2e25ad4fafc2500e1f6e596393052333867a72
unknown <lua.barbosa@yahoo.com.br>
2020-12-04 11:02:54 -0300

    feat: Inlcuindo lista de cobranças

---
+++ src/Pages/Listar cobrança/index.js
@@ -0,0 +1,17 @@
+import React, { useContext } from "react";
+import "./style.css";
+
+export default ListarCobranca(props){
+return (
+	<div className="tabela">
+	<table>
+	  <thead>
+		<th>Cliente</th>
+		<th>Descrição</th>
+		<th>Valor</th>
+		<th>Status</th>
+		<th>Vencimento</th>
+		<th>Boleto</th>
+	  </thead>
+);
+}
