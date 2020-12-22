ccda1e1209d8e6a6ed8953c5d91e536c69e92673
unknown <lua.barbosa@yahoo.com.br>
2020-12-22 13:43:29 -0300

    feat: Inlcuindo página de cobrança

--- src/Pages/Cobranças/index.js
+++ src/Pages/Cobranças/index.js
@@ -1,11 +1,34 @@
-import React from "react";
+import React, { useContext } from "react";
 import Header from "../Header";
 import PaginaPrincipal from "../Pagina Principal";
+import StoreContext from "../../components/Store/Context";
+import { fazerRequisicao } from "../../utills/fetch";
 import "./style.css";
 
-import lupa from "../../assets/lupa.svg";
+import ListarCobranca from "../Listar cobrança";
+import BuscarCobranca from "../Buscar cobrança";
 
 export default function Cobrancas() {
+  const [cobranca, setCobranca] = React.useState();
+  const [busca, setBusca] = React.useState();
+  const { token } = useContext(StoreContext);
+
+  React.useEffect(() => {
+    fazerRequisicao(
+      `https://pagamentos-back.herokuapp.com/cobrancas?${
+        busca ? `busca=${busca}&` : ""
+      }cobrancasPorPagina=10&offset=0`,
+      "GET",
+      token
+    )
+      .then((res) => res.json())
+      .then((resposta) => {
+        console.log(resposta);
+        if (resposta.status === "sucesso") {
+          setCobranca(resposta.dados.cobrancas);
+        }
+      });
+  }, [busca]);
   return (
     <div className="cobrancas">
       <PaginaPrincipal />
@@ -15,32 +38,10 @@ export default function Cobrancas() {
         </div>
         <div className="todasCobrancas">
           <div className="buscar">
-            <input placeholder="Procurar por nome, e-mail e CPF"></input>
-            <button>
-              <img src={lupa} />
-              BUSCAR
-            </button>
+            <BuscarCobranca busca={busca} setBusca={setBusca} />
           </div>
           <div className="tabela">
-            <table>
-              <thead>
-                <th>Cliente</th>
-                <th>Descrição</th>
-                <th>Valor</th>
-                <th>Status</th>
-                <th>Vencimento</th>
-                <th>Boleto</th>
-                <th></th>
-              </thead>
-              <tbody>
-                <tr>
-                  <td></td>
-                  <td></td>
-                  <td></td>
-                  <td></td>
-                </tr>
-              </tbody>
-            </table>
+            <ListarCobranca cobranca={cobranca} />
           </div>
         </div>
       </div>
